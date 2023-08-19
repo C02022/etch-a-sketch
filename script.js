5d2a7de49ab2bba26f6372e2d388b3adf64073af
Christopher Obando <chrisobando107@gmail.com>
2023-08-19 11:10:39 -0400

    Add buttons for new grid popup & turning pen on & off w/Plan for new grid button JS + small page CSS tweaks

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function createGrid() {
-    const content = document.querySelector('.content');
+    const content = document.querySelector('.grid');
 
     for (let i = 1; i < 17; i++) {;
         for (let j = 1; j < 17; j++) {
@@ -7,7 +7,7 @@ function createGrid() {
             currentSquareDiv.style.cssText = 'width: 40px; height: 40px;'
 
             currentSquareDiv.addEventListener("mouseover", () => {
-                currentSquareDiv.style.backgroundColor = "red";
+                currentSquareDiv.style.backgroundColor = "darkolivegreen";
             });
             
             content.appendChild(currentSquareDiv);
@@ -16,7 +16,18 @@ function createGrid() {
 }
 
 function main() {
+    // FEINT IDEA OF WHAT TO DO NEXT
+
+    // Create button
+        // When button is clicked (eventlistner), make sure the 
+        // button cant be clicked, send the prompt to to ask for the desired
+        // grid length (# of squares), store that answer in a variable, and then
+        // call a function that takes in the prompt variable and creates a new grid with it
+        // (Maybe refactor 'createGrid to have parameters in the loop and just have those values by default be 16?)
+
+
+
     createGrid()
 }
 
-main()
+main()  
